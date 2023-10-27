f716a34e06a620ac142d1a0b5fe2b5ef3dfdf19a
perttinen <samuli.viitala@gmail.com>
2023-10-27 17:11:46 +0300

    sdfd

--- server/index.js
+++ server/index.js
@@ -8,9 +8,14 @@ const { connectToDatabase } = require('./util/db')
 const loginRouter = require('./controllers/login')
 const userRouter = require('./controllers/user')
 
+const DIST_PATH = path.resolve(__dirname, './dist')
+const INDEX_PATH = path.resolve(DIST_PATH, 'index.html')
+
+app.use(express.static(DIST_PATH))
+app.get('*', (req, res) => res.sendFile(INDEX_PATH))
+
 app.use(cors())
 app.use(express.json())
-app.use(express.static('dist'))
 
 app.use('/login', loginRouter)
 app.use('/api/user', userRouter)
