87f14cb3acc99f12e7593625966eae854cfbaf4a
perttinen <samuli.viitala@gmail.com>
2023-11-04 20:23:19 +0200

    a bit better socket...

--- server/index.ts
+++ server/index.ts
@@ -1,49 +1,46 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import express from 'express'
-import { createServer } from 'http'
 import { Server } from 'socket.io'
 import path from 'path'
 import cors from 'cors'
 
+import utils from './util/config'
+import db from './util/db'
+import loginRouter from './controllers/login'
+import userRouter from './controllers/user'
+
 const app = express()
-app.use(cors())
-const server = createServer(app)
-const io = new Server(server, {
-	cors: {
-		origin: 'http://localhost:3000',
-		methods: ['GET', 'POST'],
-	},
-})
-const DIST_PATH = path.resolve(__dirname, '../client/build')
-
-app.use(express.static(DIST_PATH))
-
 app.use(cors())
 app.use(express.json())
 
-io.on('connection', (socket) => {
-	console.log('Client connected')
+const DIST_PATH = path.resolve(__dirname, '../client/build')
+app.use(express.static(DIST_PATH))
 
-	socket.on('event://add-user', (msg) => {
-		console.log('got', msg)
+const start = async () => {
+	try {
+		await db.connectToDatabase()
+		const expressServer = app.listen(utils.PORT, () => {
+			console.log(`Server running on port ${utils.PORT}`)
+		})
+		const io = new Server(expressServer, {
+			cors: {
+				origin:
+					process.env.NODE_ENV === 'dev'
+						? ['http://localhost:3000', 'http://127.0.0.1:3000']
+						: false,
+			},
+		})
 
-		// const payload = JSON.parse(msg);
-		// if(chatLogs[payload.roomID]){
-		// 	chatLogs[msg.roomID].push(payload.data);
-		// }
+		io.on('connection', (socket) => {
+			console.log(`User ${socket.id} connected`)
+		})
+	} catch (err) {
+		console.error('Failed to start the server:', err)
+	}
+}
 
-		socket.broadcast.emit('event://get-message', msg)
-	})
-
-	socket.on('disconnect', () => {
-		console.log('Client disconnected')
-	})
-})
-
-import utils from './util/config'
-import db from './util/db'
-
-import loginRouter from './controllers/login'
-import userRouter from './controllers/user'
+start().catch((err) => console.log('An error occurred:', err))
 
 app.use('/login', loginRouter)
 app.use('/api/user', userRouter)
@@ -58,16 +55,3 @@ app.get('/*', function (_req, res) {
 		}
 	)
 })
-
-const start = async () => {
-	try {
-		await db.connectToDatabase()
-		server.listen(utils.PORT, () => {
-			console.log(`Server running on port ${utils.PORT}`)
-		})
-	} catch (err) {
-		console.error('Failed to start the server:', err)
-	}
-}
-
-start().catch((err) => console.log('An error occurred:', err))
