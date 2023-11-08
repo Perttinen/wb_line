265f95789b88df0a55745ce17ba6ad7d88adcf76
perttinen <samuli.viitala@gmail.com>
2023-11-08 13:26:44 +0200

    password needed to login XD

--- server/controllers/login.ts
+++ server/controllers/login.ts
@@ -6,7 +6,9 @@ const router = express.Router()
 
 router.post('/', (async (req: Request<object, object, LoginUser>, res) => {
 	const body = { ...req.body }
-	const user = await User.findOne({ where: { username: body.username } })
+	const user = await User.findOne({
+		where: { username: body.username, password: body.password },
+	})
 	if (!user) {
 		return res.status(401).json({ error: 'invalid username or password' })
 	}
