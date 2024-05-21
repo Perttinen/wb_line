46053f45330096fd8a6ed1748a27f2e5d9397e10
perttinen <samuli.viitala@gmail.com>
2024-05-21 16:39:47 +0300

    aaa

--- server/index.ts
+++ server/index.ts
@@ -24,14 +24,17 @@ app.use(express.json())
 const DIST_PATH = path.resolve(__dirname, '../client/build')
 app.use(express.static(DIST_PATH))
 
+
+
 app.use('/api/departure', departureRouter)
 app.use('/api/login', loginRouter)
-app.use(tokenExtractor)
+
 app.use('/api/userlevel', userLevelRouter)
 app.use('/api/user', userRouter)
 app.use('/api/dock', dockRouter)
 app.use('/api/route', routeRouter)
 app.use('/api/stop', stopRouter)
+app.use(tokenExtractor)
 app.use('/api/ship', shipRouter)
 
 
